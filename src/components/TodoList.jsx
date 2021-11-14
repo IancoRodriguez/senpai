fe35ba50572dc480b50ff962891bfc9140429c18
IancoRodriguez <74500243+IancoRodriguez@users.noreply.github.com>
2021-11-14 20:18:28 -0300

    Add files via upload

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,10 @@
+import react from "react";
+import { TodoItem } from "./TodoItem";
+
+export function TodoList({todos, toggleTodo}){
+    return(<ul>
+        {todos.map((todo)=> (
+            <TodoItem  key={todo.id} todo= {todo} toggleTodo = {toggleTodo} />
+        ))}
+    </ul>)
+}
